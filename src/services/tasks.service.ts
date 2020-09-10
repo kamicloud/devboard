cfc2bd21989fe8091f9212cf828a0f985cf333e7
Kamicloud <cy602939074@qq.com>
2020-09-10 23:23:38 +0800

    optimize for jenkins notify

--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -3,15 +3,32 @@ import { Cron } from '@nestjs/schedule';
 import axios from 'axios';
 import _ from 'lodash';
 import { ConfigService } from '@nestjs/config';
+import notifier from 'node-notifier';
+
+const STATE_RUNNING = 'RUNNING';
+const STATE_FINISHED = 'FINISHED';
+const STATE_QUEUED = 'QUEUED';
+
+declare interface JenkinsRun {
+  id: string,
+  pipeline: string,
+  state: string,
+  changeSet: {}[],
+  result: string,
+  causes: {
+    shortDescription: string,
+  }[]
+}
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
   private temp = {};
+  private notifyEnabled = {};
 
   constructor(private configService: ConfigService) { }
 
-  @Cron('*/20 * * * * *')
+  @Cron('*/10 * * * * *')
   async handleCron() {
     const { enabled, username, password, host, jobs, dingdingToken } = this.configService.get<any>('jenkins');
 
@@ -20,33 +37,57 @@ export class TasksService {
     }
 
     await jobs.split(',').forEach(async job => {
-      const { data } = await axios.get(`${host}/blue/rest/organizations/jenkins/pipelines/${job}/runs/`, {
+      const { data }: { data: JenkinsRun[] } = await axios.get(`${host}/blue/rest/organizations/jenkins/pipelines/${job}/runs/`, {
         auth: {
           username,
           password,
         }
       });
 
-      let contentArr: string[] = data.reverse().map(element => {
-        let content = '';
+      let contentArr: string[] = data.reverse().filter(element => {
         const pipeline = decodeURIComponent(element.pipeline)
         const hash = `${pipeline}_${element.id}`;
         const state = element.state;
-        console.log(pipeline, state);
-        if (this.temp[hash] && this.temp[hash].state === state) {
-          return null;
+
+        const filter = !this.temp[hash] || this.temp[hash].state !== state;
+
+        if (filter) {
+          this.temp[hash] = element;
+
         }
-        this.temp[hash] = element;
-        content += `${pipeline} \t${state}`;
-        // if (element.endTime) {
-        // content += `, Finished At: ${element.endTime}`;
-        // }
-        content += "\n";
+        return filter;
+      }).filter(element => {
+        const changeSet = element.changeSet;
+        if (this.notifyEnabled[job] && changeSet && changeSet.length) {
+          notifier.notify(JSON.stringify(element));
+          return false;
+        }
+
+        return true;
+      }).filter(element => {
+        const { causes } = element;
+
+        for (let i = 0; i < causes.length; i++) {
+          const cause = causes[i];
+
+          if (cause.shortDescription.indexOf('Push event to branch') !== -1) {
+            return false;
+          }
+        }
+
+        return true;
+      }).map(element => {
+        const pipeline = decodeURIComponent(element.pipeline)
+        const { state, result } = element;
+
+        let content = '';
+
+        content += `${pipeline} \t${this.mapState(state)}\t${this.mapResult(result)}`;
 
         return content;
-      }).filter(a => a);
-      if (contentArr.length) {
-        const content = "Releases:\n" + contentArr.join("==================\n");
+      });
+      if (dingdingToken && this.notifyEnabled[job] && contentArr.length) {
+        const content = "Releases:\n" + contentArr.join("\n");
         await axios.post(`https://oapi.dingtalk.com/robot/send?access_token=${dingdingToken}`, {
           msgtype: 'text',
           text: {
@@ -54,6 +95,33 @@ export class TasksService {
           }
         });
       }
+      this.notifyEnabled[job] = true;
     });
+  }
+
+  public mapResult(result: string) {
+    switch (result) {
+      case 'UNKNOWN':
+        return '未完成';
+      case 'SUCCESS':
+        return '成功';
+      case 'FAILURE':
+        return '失败';
+      default:
+        return result;
+    }
+  }
+
+  public mapState(state: string) {
+    switch (state) {
+      case 'FINISHED':
+        return '部署结束';
+      case 'RUNNING':
+        return '部署中';
+      case 'QUEUED':
+        return '队列等待'
+      default:
+        return state;
+    }
   }
 }
